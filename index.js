d29e138facf8fe40bcd25d01da73782800452564
Zak Grivell <zak@grivell.uk>
2025-10-25 12:55:49 +0100

    init

---
+++ index.js
@@ -0,0 +1,79 @@
+/** @type {HTMLCanvasElement} */
+let canvas = document.getElementById("game");
+let ctx = canvas.getContext("2d");
+
+let tetris_grid = {};
+
+let images = {
+  I: new Image(),
+  J: new Image(),
+  L: new Image(),
+  O: new Image(),
+  S: new Image(),
+  T: new Image(),
+  Z: new Image(),
+};
+
+function p(x, y) {
+  return { x, y };
+}
+
+let block_types = "IJLOSTZ";
+
+let block_shapes = {
+  I: [p(0, 0), p(0, 1), p(0, 2), p(0, 3)],
+  J: [p(1, 0), p(1, 1), p(1, 2), p(0, 2)],
+  L: [p(0, 0), p(0, 1), p(0, 2), p(1, 2)],
+  O: [p(0, 0), p(0, 1), p(1, 0), p(1, 1)],
+  S: [p(0, 0), p(0, 1), p(1, 1), p(1, 2)],
+  T: [p(1, 0), p()],
+};
+
+Object.keys(images).forEach((k) => {
+  images[k].src = `assets/${k}.png`;
+});
+
+function render_block(block) {
+  ctx.drawImage(
+    images[block.t],
+    block.pos.x * 16,
+    block.pos.y * 16,
+    block.size.x * 16,
+    block.size.y * 16,
+  );
+}
+
+const X_SIZE = 10;
+const Y_SIZE = 40;
+
+/** @type {{{ x: number, y:number}: {t:string, pos: {x:number, y:number}}}} */
+let grid = {};
+
+function on_load() {
+  window.requestAnimationFrame(() => {});
+
+  let current_down = {
+    t: "I",
+    pos: { x: 0, y: 0 },
+    size: { x: 1, y: 4 },
+  };
+
+  setInterval(() => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    if (current_down.pos.y + current_down.size.y == Y_SIZE) {
+      blocks[current_down.pos] = current_down;
+
+      current_down = {
+        t: "O",
+        pos: { x: 0, y: 0 },
+        size: { x: 2, y: 2 },
+      };
+    }
+
+    Object.values(blocks).forEach(render_block);
+
+    render_block(current_down);
+    current_down.pos.y += 1;
+  }, 500);
+}
